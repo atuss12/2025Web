23006619828c3503f3f78ccb1f294809c076fef0
atuss12 <halimatussadiahbtl@gmail.com>
2025-03-10 13:12:15 +0800

    membuat file prodi.php

--- index.php
+++ index.php
@@ -5,9 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SIMPADU POLIBAN</title>
 </head>
-<body>
-    <h1>DATA MAHASISWA</h1>
+<h1>Data Mahasiswa</h1>
     <br>
-    <?php echo "ini data mahasiswa";?>
+    <table border ="1" cellspacing = "0" cellpadding = "5">
+        <thead>
+            <th>No</th>
+            <th>NIM</th>
+            <th>NAMA</th>
+        </thead>
+</body>
+<tr>
+    <td>1</td>
+    <td>E020323028</td>
+    <td>Atuss</td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>E020323029</td>
+    <td>Halimah</td>
+</tr>
+<tr>
+    <td>1</td>
+    <td>E020323030</td>
+    <td>halim</td>
+</tr>
+</table>
 </body>
 </html>
